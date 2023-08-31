f6ac4fb336f44afa508ca10e77a2d5d81ac8042b
cfzimmerman <coryzimmerman93@gmail.com>
2023-08-30 20:47:49 -0500

    start ps0

--- ps0/ps0.py
+++ ps0/ps0.py
@@ -4,14 +4,18 @@
 #               #
 #################
 
+from typing import Optional
 
 #
 # Setup
 #
+
+
 class BinaryTree:
     def __init__(self, root):
         self.root: BTvertex = root
- 
+
+
 class BTvertex:
     def __init__(self, key):
         self.parent: BTvertex = None
@@ -19,6 +23,7 @@ class BTvertex:
         self.right: BTvertex = None
         self.key: int = key
         self.size: int = None
+
 
 #
 # Problem 1a
@@ -29,9 +34,16 @@ class BTvertex:
 # Side effect: sets the size of each vertex n in the
 # ... tree rooted at vertex v to the size of that subtree
 # Runtime: O(n)
-def calculate_sizes(v):
-    # Your code goes here
-    pass
+
+
+def calculate_sizes(v: Optional[BTvertex]) -> int:
+    if v is None:
+        return 0
+    left = calculate_sizes(v.left)
+    right = calculate_sizes(v.right)
+    v.size = left + right + 1
+    return v.size
+
 
 #
 # Problem 1c
@@ -42,6 +54,18 @@ def calculate_sizes(v):
 # Output: A BTvertex that, if removed from the tree, would result
 # ... in disjoint trees that all have at most n/2 vertices
 # Runtime: O(h)
-def find_vertex(r): 
-    # Your code goes here
-    pass
+
+
+def find_vertex(r: Optional[BTvertex]) -> Optional[BTvertex]:
+    if r is None:
+        return r
+    half = r.size // 2
+    while r:
+        if r.left and r.left.size > half:
+            r = r.left
+            continue
+        if r.right and r.right.size > half:
+            r = r.right
+            continue
+        return r
+    raise Exception("failed to find a solution")
--- ps0/ps0_tests.py
+++ ps0/ps0_tests.py
@@ -1,18 +1,22 @@
-from ps0 import BinaryTree,BTvertex,calculate_sizes,find_vertex
+from ps0 import BinaryTree, BTvertex, calculate_sizes, find_vertex
+
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+
 
 # Generates a full/balanced binary tree of `height`
-def gen_tree(height, counter = {"val": 1}):
+
+
+def gen_tree(height, counter={"val": 1}):
     def gen_vertex(height, counter=counter):
         # Counter needs to be an obj to pass ref
         # Counter needs to be a param if modified or it will be local
@@ -21,6 +25,7 @@ def gen_tree(height, counter = {"val": 1}):
             # increment counter for key
             counter["val"] += 1
             return root
+
         def gen_tree_inner(height):
             if height == 1:
                 return create_tree_node()
@@ -31,22 +36,31 @@ def gen_tree(height, counter = {"val": 1}):
                 root.right = gen_vertex(height - 1)
                 root.right.parent = root
                 return root
+
         return gen_tree_inner(height)
+
     return BinaryTree(gen_vertex(height))
 
+
 # Removes the outmost branch at `depth` and `side`
-def remove_branch(T, depth = 1, side="left"):
+
+
+def remove_branch(T, depth=1, side="left"):
     def remove_branch_inner(vertex, depth, side):
         if depth == 1:
             setattr(vertex, side, None)
         else:
             remove_branch_inner(getattr(vertex, side), depth - 1, side)
         return vertex
+
     remove_branch_inner(T.root, depth, side)
     return T
 
+
 # Returns true if removing `v` from the tree rooted at `root`
 # ... would result in disjoint trees that all have at most n/2 vertices
+
+
 def validate_found_vert(root, v):
     threshold = root.size / 2
     t1 = v.left.size if v.left else 0
@@ -54,10 +68,10 @@ def validate_found_vert(root, v):
     t3 = root.size - v.size
     return max([t1, t2, t3]) <= threshold
 
+
 def test():
     tests_a = [
         # Format: (input, postprocessing, expected result)
-
         # Basic Tests
         (gen_tree(1).root, (lambda root: root), 1),
         (gen_tree(2).root, (lambda root: root), 3),
@@ -77,15 +91,19 @@ def test():
         (remove_branch(gen_tree(8), 1).root, (lambda root: root), 255 - 127),
     ]
     print("Problem 1a Tests:")
-    for i,test in enumerate(tests_a):
+    for i, test in enumerate(tests_a):
         calculate_sizes(test[0])
         result = test[1](test[0])
         score = result and hasattr(result, "size") and result.size == test[2]
-        print("Test " + str(i + 1) + ": ", f"{bcolors.OKGREEN}Passed{bcolors.ENDC}" if score else f"{bcolors.FAIL}Failed{bcolors.ENDC}")
+        print(
+            "Test " + str(i + 1) + ": ",
+            f"{bcolors.OKGREEN}Passed{bcolors.ENDC}"
+            if score
+            else f"{bcolors.FAIL}Failed{bcolors.ENDC}",
+        )
 
     tests_c = [
         # Format: input
-
         # Basic tests
         gen_tree(3).root,
         gen_tree(4).root,
@@ -98,13 +116,19 @@ def test():
         remove_branch(gen_tree(8), 1).root,
         remove_branch(gen_tree(8), 1).root,
     ]
-    
+
     print()
     print("Problem 1c Tests:")
-    for i,test in enumerate(tests_c):
+    for i, test in enumerate(tests_c):
         calculate_sizes(test)
         result = find_vertex(test)
-        print("Test " + str(i + 1) + ": ", f"{bcolors.OKGREEN}Passed{bcolors.ENDC}" if validate_found_vert(test, result) else f"{bcolors.FAIL}Failed{bcolors.ENDC}")
+        print(
+            "Test " + str(i + 1) + ": ",
+            f"{bcolors.OKGREEN}Passed{bcolors.ENDC}"
+            if validate_found_vert(test, result)
+            else f"{bcolors.FAIL}Failed{bcolors.ENDC}",
+        )
+
 
 if __name__ == "__main__":
     test()
