ac92cb53108d39daca2e7b5a9fff845ff0d11120
cfzimmerman <coryzimmerman93@gmail.com>
2023-10-15 17:43:13 -0400

    add ps4

--- ps3/simulator.py
+++ ps3/simulator.py
@@ -49,27 +49,22 @@ def executeProgram(programArr, inputArr):
             memory[variableList[ops[0]]] = variableList[ops[1]]
         if cmd == "assign":
             # ['assign', i, j]: assign var_i to the value j
-            # TODO: implement assign
             variableList[ops[0]] = ops[1]
 
         # Arithmetic commands
         if cmd == "+":
             # ['+', i, j, k]: compute (var_j + var_k) and store in var_i
-            # TODO: Implement addition.
             variableList[ops[0]] = variableList[ops[1]] + variableList[ops[2]]
         if cmd == "-":
             # ['-', i, j, k]: compute max((var_j - var_k), 0) and store in var_i.
-            # TODO: Implement subtraction.
             variableList[ops[0]] = max(variableList[ops[1]] - variableList[ops[2]], 0)
         if cmd == "*":
             # ['*', i, j, k]: compute (var_j * var_k) and store in var_i.
-            # TODO: Implement multiplication.
             variableList[ops[0]] = variableList[ops[1]] * variableList[ops[2]]
         if cmd == "/":
             #  ['/', i, j, k]: compute (var_j // var_k) and store in var_i.
             # Note that this is integer division. You should return an integer, not a float.
             # Remember division by 0 results in 0.
-            # TODO: Implement division.
             if variableList[ops[2]] == 0:
                 variableList[ops[0]] = 0
             else:
@@ -80,7 +75,6 @@ def executeProgram(programArr, inputArr):
         # Control commands
         if cmd == "goto":
             # ['goto', i, j]: if var_i is equal to 0, go to line j
-            # TODO: Implement goto.
             if variableList[ops[0]] == 0:
                 programCounter = ops[1]
                 continue
