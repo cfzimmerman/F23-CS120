708bfb500a2f5689552beb09396d4765edaf150f
cfzimmerman <coryzimmerman93@gmail.com>
2023-11-12 21:06:05 -0500

    sat 3 coloring

--- ps7/ps7.py
+++ ps7/ps7.py
@@ -1,35 +1,43 @@
 from itertools import product, combinations
 from pysat.solvers import Glucose3
+from typing import List, Set, Tuple
 
-'''
+"""
 Before you start: Read the README and the Graph implementation below.
-'''
+"""
+
 
 class Graph:
-    '''
+    """
     A graph data structure with number of nodes N, list of sets of edges, and a list of color labels.
     Nodes and colors are both 0-indexed.
     For a given node u, its edges are located at self.edges[u] and its color is self.color[u].
-    '''
+    """
 
     # Initializes the number of nodes, sets of edges for each node, and colors
-    def __init__(self, N, edges = None, colors = None):
+    def __init__(self, N, edges=None, colors=None):
         self.N = N
-        self.edges = [set(lst) for lst in edges] if edges is not None else [set() for _ in range(N)]
-        self.colors = [c for c in colors] if colors is not None else [None for _ in range(N)]
-    
+        self.edges: List[Set[int]] = (
+            [set(lst) for lst in edges]
+            if edges is not None
+            else [set() for _ in range(N)]
+        )
+        self.colors = (
+            [c for c in colors] if colors is not None else [None for _ in range(N)]
+        )
+
     # Adds a node to the end of the list
     # Returns resulting graph
     def add_node(self):
         self.N += 1
         self.edges.append(set())
         return self
-    
+
     # Adds an undirected edge from u to v
     # Returns resulting graph
     def add_edge(self, u, v):
-        assert(v not in self.edges[u])
-        assert(u not in self.edges[v])
+        assert v not in self.edges[u]
+        assert u not in self.edges[v]
         self.edges[u].add(v)
         self.edges[v].add(u)
         return self
@@ -37,8 +45,8 @@ class Graph:
     # Removes the undirected edge from u to v
     # Returns resulting graph
     def remove_edge(self, u, v):
-        assert(v in self.edges[u])
-        assert(u in self.edges[v])
+        assert v in self.edges[u]
+        assert u in self.edges[v]
         self.edges[u].remove(v)
         self.edges[v].remove(u)
         return self
@@ -53,9 +61,9 @@ class Graph:
         return Graph(self.N, self.edges, self.colors)
 
     def clone_and_merge(self, g2, g1u, g2v):
-        '''
+        """
         DOES NOT COPY COLORS
-        '''
+        """
         g1 = self
         edges = g1.edges + [[v + g1.N for v in u_list] for u_list in g2.edges]
         g = Graph(g1.N + g2.N, edges)
@@ -63,7 +71,7 @@ class Graph:
             g = g.add_edge(g1u, g2v + g1.N)
         return g
 
-    # Checks if a given subset of nodes is an independent set 
+    # Checks if a given subset of nodes is an independent set
     def is_independent_set(self, subset):
         for v in subset:
             for u in self.edges[v]:
@@ -75,7 +83,6 @@ class Graph:
     def is_graph_coloring_valid(self):
         for u in range(self.N):
             for v in self.edges[u]:
-
                 # Check if every one has a coloring
                 if self.colors[u] is None or self.colors[v] is None:
                     return False
@@ -83,20 +90,22 @@ class Graph:
                 # Make sure colors on each edge are different
                 if self.colors[u] == self.colors[v]:
                     return False
-        
+
         return True
 
-'''
+
+"""
     Introduction: We've implemented exhaustive search for you below.
     You don't need to implement any extra code for this part.
-'''
+"""
 
 # Given an instance of the Graph class G, exhaustively search for a k-coloring
 # Returns the coloring list if one exists, None otherwise.
-def exhaustive_search_coloring(G, k=3):
 
+
+def exhaustive_search_coloring(G, k=3):
     # Iterate through every possible coloring of nodes
-    for coloring in product(range(1,k+1), repeat=G.N):
+    for coloring in product(range(1, k + 1), repeat=G.N):
         G.colors = list(coloring)
         if G.is_graph_coloring_valid():
             return G.colors
@@ -106,11 +115,10 @@ def exhaustive_search_coloring(G, k=3):
     return None
 
 
-
-'''
+"""
     We've implemented bfs_2_coloring for you below.
     You don't need to implement any extra code for this part.
-'''
+"""
 
 # Given an instance of the Graph class G and a subset of precolored nodes,
 # Assigns precolored nodes to have color 2, and attempts to color the rest using colors 0 and 1.
@@ -118,6 +126,8 @@ def exhaustive_search_coloring(G, k=3):
 # Precondition: Assumes that the precolored_nodes form an independent set.
 # If successful, modifies G.colors and returns the coloring.
 # If no coloring is possible, resets all of G's colors to None and returns None.
+
+
 def bfs_2_coloring(G, precolored_nodes=None):
     # Assign every precolored node to have color 2
     # Initialize visited set to contain precolored nodes if they exist
@@ -152,15 +162,18 @@ def bfs_2_coloring(G, precolored_nodes=None):
 
     if G.is_graph_coloring_valid():
         return G.colors
-    
+
     G.reset_colors()
     return None
 
-'''
+
+"""
     We've implemented iset_bfs_3_coloring for you below.
     You don't need to implement any extra code for this part.
-'''
+"""
 # Given an instance of the Graph class G and a subset of precolored nodes, searches for a 3 coloring
+
+
 def iset_bfs_3_coloring(G):
     for set_size in range(G.N // 3 + 1):
         for combination in combinations(range(G.N), set_size):
@@ -171,7 +184,8 @@ def iset_bfs_3_coloring(G):
                     return coloring
     return None
 
-'''
+
+"""
     Part A: Implement the reduction to SAT. 
     Here, you should use the SAT solver that we've defined to add clauses, and use the built-in get_model function
     to find the solution if one exists.
@@ -182,7 +196,21 @@ def iset_bfs_3_coloring(G):
     3. Transform the solver output into a valid coloring if one exists, else return None.
     When you're finished, check your work by running python3 -m ps8_color_tests 3.
     Don't worry if some of your tests time out: that is expected.
-'''
+"""
+
+three_colors = [1, 2, 3]
+
+# Encodes a node as a color
+
+
+def encolor(node: int, color: int) -> int:
+    return node * 10 + color
+
+
+# Decodes a (node, color) pair
+def decolor(encolored: int) -> Tuple[int, int]:
+    return (encolored // 10, encolored % 10)
+
 
 # Given an instance of the Graph class G, reduces 3 coloring to SAT
 # If successful, modifies G.colors and returns the coloring.
@@ -190,23 +218,41 @@ def iset_bfs_3_coloring(G):
 def sat_3_coloring(G):
     solver = Glucose3()
 
-    # TODO: Add the clauses to the solver
+    # every node must have a color
+    for node in range(G.N):
+        clause = [encolor(node, color) for color in three_colors]
+        solver.add_clause(clause)
 
-    # Attempt to solve, return None if no solution possible
+    # the endpoints of an edge cannot have the same color
+    for node, edges in enumerate(G.edges):
+        for neighbor in edges:
+            for color in three_colors:
+                clause = [
+                    -encolor(node, color),
+                    -encolor(neighbor, color),
+                ]
+                solver.add_clause(clause)
+
+        # Attempt to solve, return None if no solution possible
     if not solver.solve():
         G.reset_colors()
         return None
 
     # Accesses the model in form [-v1, v2, -v3 ...], which denotes v1 = False, v2 = True, v3 = False, etc.
     solution = solver.get_model()
-
-    # TODO: If a solution is found, convert it into a coloring and update G.colors
+    for assignment in solution:
+        if assignment < 0:
+            continue
+        (node, color) = decolor(assignment)
+        G.colors[node] = color
 
     return G.colors
+
 
 # Feel free to add miscellaneous tests below!
 if __name__ == "__main__":
     G0 = Graph(2).add_edge(0, 1)
     print(bfs_2_coloring(G0))
     print(iset_bfs_3_coloring(G0))
+    print(sat_3_coloring(G0))
     print(sat_3_coloring(G0))
